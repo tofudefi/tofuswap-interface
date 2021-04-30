be601287690df3f0189a3ff014114ab0707ddb31
TofuDefi <72276741+tofudefi@users.noreply.github.com>
2021-04-30 23:20:50 +0800

    increase chunk size to four

--- src/state/multicall/updater.tsx
+++ src/state/multicall/updater.tsx
@@ -17,7 +17,7 @@ import {
 } from './actions'
 
 // chunk calls so we do not exceed the gas limit
-const CALL_CHUNK_SIZE = 2
+const CALL_CHUNK_SIZE = 4
 
 /**
  * Fetches a chunk of calls, enforcing a minimum block number constraint
