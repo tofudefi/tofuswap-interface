7d4fd03ca9ff4dc959232042bf9595badc0e1ce7
TofuDefi <72276741+tofudefi@users.noreply.github.com>
2021-05-30 17:33:01 +0800

    use tron address as currencyId

--- src/utils/currencyId.ts
+++ src/utils/currencyId.ts
@@ -1,7 +1,8 @@
 import { Currency, TRX, Token } from '@tofudefi/tofuswap-sdk'
+import { ethAddress } from '@tofudefi/java-tron-provider'
 
 export function currencyId(currency: Currency): string {
   if (currency === TRX) return 'TRX'
-  if (currency instanceof Token) return currency.address
+  if (currency instanceof Token) return ethAddress.toTron(currency.address)
   throw new Error('invalid currency')
 }
