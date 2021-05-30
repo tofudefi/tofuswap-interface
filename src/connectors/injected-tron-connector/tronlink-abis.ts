b53599e552d79e6a58ecf8b1a5d93ced5b4111e7
TofuDefi <72276741+tofudefi@users.noreply.github.com>
2021-05-30 17:42:23 +0800

    add staking rewards abis

--- src/connectors/injected-tron-connector/tronlink-abis.ts
+++ src/connectors/injected-tron-connector/tronlink-abis.ts
@@ -3,6 +3,8 @@ import { V1_EXCHANGE_ABI, V1_FACTORY_ABI } from '../../constants/v1'
 import { abi as ITofuswapV2PairABI } from '@tofudefi/tofuswap-v2-core/build/ITofuswapV2Pair.json'
 import { abi as ITofuswapV2Factory } from '@tofudefi/tofuswap-v2-core/build/ITofuswapV2Factory.json'
 import { abi as ITofuswapV2Router02ABI } from '@tofudefi/tofuswap-v2-periphery/build/ITofuswapV2Router02.json'
+import { abi as STAKING_REWARDS_ABI } from '@uniswap/liquidity-staker/build/StakingRewards.json'
+import { abi as STAKING_REWARDS_FACTORY_ABI } from '@uniswap/liquidity-staker/build/StakingRewardsFactory.json'
 import ENS_ABI from '../../constants/abis/ens-registrar.json'
 import ENS_PUBLIC_RESOLVER_ABI from '../../constants/abis/ens-public-resolver.json'
 import UNISOCKS_ABI from '../../constants/abis/unisocks.json'
@@ -23,5 +25,7 @@ export const abis = [
   ...UNISOCKS_ABI,
   ...WETH_ABI,
   ...MIGRATOR_ABI,
-  ...MULTICALL_ABI
+  ...MULTICALL_ABI,
+  ...STAKING_REWARDS_ABI,
+  ...STAKING_REWARDS_FACTORY_ABI
 ]
