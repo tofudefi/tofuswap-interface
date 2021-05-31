830101979ed1ac9bce9f1c9482e0412b13dee605
TofuDefi <72276741+tofudefi@users.noreply.github.com>
2021-05-31 20:51:35 +0800

    disable GA

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core'
 import 'inter-ui'
 import React, { StrictMode } from 'react'
-import { isMobile } from 'react-device-detect'
+//import { isMobile } from 'react-device-detect'
 import ReactDOM from 'react-dom'
-import ReactGA from 'react-ga'
+//import ReactGA from 'react-ga'
 import { Provider } from 'react-redux'
 import { HashRouter } from 'react-router-dom'
 import Blocklist from './components/Blocklist'
@@ -27,6 +27,7 @@ if ('ethereum' in window) {
 }
 */
 
+/*
 const GOOGLE_ANALYTICS_ID: string | undefined = process.env.REACT_APP_GOOGLE_ANALYTICS_ID
 if (typeof GOOGLE_ANALYTICS_ID === 'string') {
   ReactGA.initialize(GOOGLE_ANALYTICS_ID)
@@ -37,12 +38,14 @@ if (typeof GOOGLE_ANALYTICS_ID === 'string') {
   ReactGA.initialize('test', { testMode: true, debug: true })
 }
 
+
 window.addEventListener('error', error => {
   ReactGA.exception({
     description: `${error.message} @ ${error.filename}:${error.lineno}:${error.colno}`,
     fatal: true
   })
 })
+*/
 
 function Updaters() {
   return (
